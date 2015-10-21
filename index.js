3538daecd1b1a88a340f40dac97c2f196cca4de8
npateriya <npateriy@cisco.com>
2015-10-20 21:43:05 -0700

    Updating index to tropo

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+/**
+ * Showing with the Express framwork http://expressjs.com/
+ * Express must be installed for this sample to work
+ */
+
+var tropowebapi = require('./lib/tropo-webapi');
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello from Cisco Shipped!');
+app.post('/', function(req, res){
+	// Create a new instance of the TropoWebAPI object.
+	var tropo = new tropowebapi.TropoWebAPI();
+	// Use the say method https://www.tropo.com/docs/webapi/say.htm
+	tropo.say("Hello World!");
+
+    res.send(tropowebapi.TropoJSON(tropo));
 });
 
-var server = app.listen(3000, function () {
 
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-
-});
+app.listen(3000);
+console.log('Server running on http://0.0.0.0:3000/')
