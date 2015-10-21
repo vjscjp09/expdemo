12ee244d4e80f8987f86883c04e5832a29f9320f
npateriya <npateriy@cisco.com>
2015-10-20 21:58:22 -0700

    Adding / for health

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello from Cisco Shipped!');
+});
+
+
 app.post('/', function(req, res){
 	// Create a new instance of the TropoWebAPI object.
 	var tropo = new tropowebapi.TropoWebAPI();
